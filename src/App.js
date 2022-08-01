eb336c3328a7fca1e61a42e099e80a6e6c858c31
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-01 21:07:37 +0300

    imported all components into the app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-// importing the browser router
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import Registration from './components/Registration';
-import Login from './components/Login';
-import TodoList from './components/TodoList';
+// // importing the browser router
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import React from 'react';
+// import './App.css';
+// import Home from './components/Home';
+// import Registration from './components/Registration';
+// import Login from './components/Login';
+// import TodoList from './components/TodoList';
 
-function App() {
+// function App() {
   
-  return (
-    <div className="App">
+//   return (
+//     <div className="App">
 
-      <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<App />} />
-        <Route path ="/Registration" element={<Registration />} />
-        <Route path ="/Login" element={<Login />} />
-        <Route path ="/TodoList" element={<TodoList/>}/>
-      </Routes>
-    </Router>
-    </div>
-  );
-}
+//       <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/" element={<App />} />
+//         <Route path ="/Registration" element={<Registration />} />
+//         <Route path ="/Login" element={<Login />} />
+//         <Route path ="/TodoList" element={<TodoList/>}/>
+//       </Routes>
+//     </Router>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
